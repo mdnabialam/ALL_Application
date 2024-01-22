87a7cdfa80312a024fb96d59fffdcd11a7760101
MD NABI ALAM <nabialamspring.com>
2024-01-22 12:42:00 +0530

    Flux Multipal obj

---
+++ Flux_Multipal_Object_01_use_Stock/src/main/java/com/sbms/alam/server/FluxMultipalObject01UseStockApplication.java
@@ -0,0 +1,13 @@
+package com.sbms.alam.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxMultipalObject01UseStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxMultipalObject01UseStockApplication.class, args);
+	}
+
+}
---
+++ Flux_Multipal_Object_01_use_Stock/src/main/java/com/sbms/alam/server/entity/CustomerEvent.java
@@ -0,0 +1,20 @@
+package com.sbms.alam.server.entity;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerEvent {
+
+	private String name;
+	private Date createDate;
+	
+	
+	
+	
+}
