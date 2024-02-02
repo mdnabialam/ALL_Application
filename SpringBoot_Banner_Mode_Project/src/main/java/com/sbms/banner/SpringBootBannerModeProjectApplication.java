fca1094c46bf7e2c015ea6ca57ad3f2501699250
MD NABI ALAM <nabialamspring.com>
2024-02-02 15:48:54 +0530

    banner

---
+++ SpringBoot_Banner_Mode_Project/src/main/java/com/sbms/banner/SpringBootBannerModeProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sbms.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBannerModeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBannerModeProjectApplication.class, args);
+		System.out.println("New  Banner ");
+	}
+
+}
